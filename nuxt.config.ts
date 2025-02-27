605e86b999238919256e289200e40e101457fe76
ronger <ronger-x@outlook.com>
2025-02-27 08:13:07 +0800

    :heavy_plus_sign: auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,7 @@ export default defineNuxtConfig({
     '@nuxt/eslint',
     '@nuxt/ui-pro',
     '@vueuse/nuxt',
-    '@sidebase/nuxt-auth',
-    '@logto/nuxt'
+    '@sidebase/nuxt-auth'
   ],
 
   devtools: {
